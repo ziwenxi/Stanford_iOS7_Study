5b4c7644354d92d35455cf4c7e6e1502127327ce
NSLogMeng <nslogmeng@163.com>
2015-05-18 18:51:09 +0800

    Lecture-1

--- Machismo/Model/Card.h
+++ Machismo/Model/Card.h
@@ -9,5 +9,9 @@
 #import <Foundation/Foundation.h>
 
 @interface Card : NSObject
+@property (strong,nonatomic) NSString *contents;
+@property (nonatomic,getter=isChosen) BOOL chosen;
+@property (nonatomic,getter=isMacthed) BOOL matched;
 
+- (int)match:(NSArray *)otherCards;
 @end
